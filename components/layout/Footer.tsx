4ef243e4966994529d7d822021319de368eb205e
Sampathwran <sampathwran@gmail.com>
2026-09-25 02:29:25 +0530

    Specify from email address for automated newsletter subscription emails

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -29,6 +29,7 @@ export default function Footer() {
       // 2. Add to mail collection to trigger automated email
       await addDoc(collection(db, 'mail'), {
         to: email,
+        from: 'FlightChap <info@flightchap.com>',
         message: {
           subject: 'Thank you for subscribing to FlightChap!',
           html: '<div style="font-family: sans-serif; max-width: 600px; margin: 0 auto;"><h2>Welcome to FlightChap! ??</h2><p>You have successfully subscribed to our exclusive fare alerts and travel newsletter.</p><p>We will make sure you are the first to know about secret flight deals and premium offers.</p><br/><p>Best Regards,<br/>The FlightChap Team</p></div>'
